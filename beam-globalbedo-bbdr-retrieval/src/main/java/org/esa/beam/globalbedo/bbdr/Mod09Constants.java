f07a0ac825f5b60b7a3dd35a1dc089089ddec8ba
dolaf <olaf.danne@brockmann-consult.de>
2016-02-24 11:34:10 +0100

    continued extraction of MOD09 state band

--- beam-globalbedo-bbdr-retrieval/src/main/java/org/esa/beam/globalbedo/bbdr/Mod09Constants.java
+++ beam-globalbedo-bbdr-retrieval/src/main/java/org/esa/beam/globalbedo/bbdr/Mod09Constants.java
@@ -1,5 +1,7 @@
 package org.esa.beam.globalbedo.bbdr;
 
+import java.awt.*;
+
 /**
  * todo: add comment
  * To change this template use File | Settings | File Templates.
@@ -13,15 +15,115 @@ public class Mod09Constants {
     public static final String MOD09_STATE_BAND_NAME = "state_1km_1";
     public static final String MOD09_STATE_FLAG_BAND_NAME = "STATE_FLAGS";
 
-    public static final int STATE_CLOUD_BIT_INDEX = 0;
-    public static final int STATE_CLOUD_SHADOW_BIT_INDEX = 1;
-    public static final int STATE_LAND_WATER_BIT_INDEX = 2;
-    public static final int STATE_AEROSOL_QUANTITY_BIT_INDEX = 3;
-    public static final int STATE_CIRRUS_BIT_INDEX = 4;
-    public static final int STATE_INTERNAL_CLOUD_ALGORITHM_BIT_INDEX = 5;
-    public static final int STATE_INTERNAL_FIRE_ALGORITHM_BIT_INDEX = 6;
-    public static final int STATE_MOD35_SNOW_ICE_BIT_INDEX = 7;
-    public static final int STATE_CLOUD_ADJACENCY_BIT_INDEX = 8;
-    public static final int STATE_SALT_PAN_BIT_INDEX = 9;
-    public static final int STATE_INTERNAL_SNOW_MASK_BIT_INDEX = 10;
+    public static final int STATE_CLEAR_BIT_INDEX = 0;
+    public static final int STATE_CLOUD_BIT_INDEX = 1;
+    public static final int STATE_CLOUD_MIXED_BIT_INDEX = 2;
+    public static final int STATE_CLOUD_UNKNOWN_BIT_INDEX = 3;
+    public static final int STATE_CLOUD_SHADOW_BIT_INDEX = 4;
+
+    public static final int STATE_SHALLOW_OCEAN_BIT_INDEX = 5;
+    public static final int STATE_LAND_BIT_INDEX = 6;
+    public static final int STATE_COASTLINE_BIT_INDEX = 7;
+    public static final int STATE_SHALLOW_INLAND_WATER_BIT_INDEX = 8;
+    public static final int STATE_EPHEMERAL_WATER_BIT_INDEX = 9;
+    public static final int STATE_DEEP_INLAND_WATER_BIT_INDEX = 10;
+    public static final int STATE_MODERATE_OCEAN_BIT_INDEX = 11;
+    public static final int STATE_DEEP_OCEAN_BIT_INDEX = 12;
+
+    public static final int STATE_AEROSOL_CLIMATOLOGY_BIT_INDEX = 13;
+    public static final int STATE_AEROSOL_LOW_BIT_INDEX = 14;
+    public static final int STATE_AEROSOL_AVERAGE_BIT_INDEX = 15;
+    public static final int STATE_AEROSOL_HIGH_BIT_INDEX = 16;
+
+    public static final int STATE_CIRRUS_NONE_BIT_INDEX = 17;
+    public static final int STATE_CIRRUS_SMALL_BIT_INDEX = 18;
+    public static final int STATE_CIRRUS_AVERAGE_BIT_INDEX = 19;
+    public static final int STATE_CIRRUS_HIGH_BIT_INDEX = 20;
+
+    public static final int STATE_INTERNAL_CLOUD_ALGORITHM_BIT_INDEX = 21;
+    public static final int STATE_INTERNAL_FIRE_ALGORITHM_BIT_INDEX = 22;
+    public static final int STATE_MOD35_SNOW_ICE_BIT_INDEX = 23;
+    public static final int STATE_CLOUD_ADJACENCY_BIT_INDEX = 24;
+    public static final int STATE_SALT_PAN_BIT_INDEX = 25;
+    public static final int STATE_INTERNAL_SNOW_MASK_BIT_INDEX = 26;
+
+
+    public static final String STATE_CLEAR_FLAG_NAME = "CLEAR";
+    public static final String STATE_CLOUD_FLAG_NAME = "CLOUDY";
+    public static final String STATE_CLOUD_MIXED_FLAG_NAME = "CLOUD_MIXED";
+    public static final String STATE_CLOUD_UNKNOWN_FLAG_NAME = "CLOUD_UNKNOWN";
+    public static final String STATE_CLOUD_SHADOW_FLAG_NAME = "CLOUD_SHADOW";
+
+    public static final String STATE_SHALLOW_OCEAN_FLAG_NAME = "SHALLOW_OCEAN";
+    public static final String STATE_LAND_FLAG_NAME = "LAND";
+    public static final String STATE_COASTLINE_FLAG_NAME = "COASTLINE";
+    public static final String STATE_SHALLOW_INLAND_WATER_FLAG_NAME = "SHALLOW_INLAND_WATER";
+    public static final String STATE_EPHEMERAL_WATER_FLAG_NAME = "EPHEMERAL";
+    public static final String STATE_DEEP_INLAND_WATER_FLAG_NAME = "DEEP_INLAND_WATER";
+    public static final String STATE_MODERATE_OCEAN_FLAG_NAME = "MODERATE_OCEAN";
+    public static final String STATE_DEEP_OCEAN_FLAG_NAME = "DEEP_OCEAN";
+
+    public static final String STATE_AEROSOL_CLIMATOLOGY_FLAG_NAME = "AEROSOL_CLIMATOLOGY";
+    public static final String STATE_AEROSOL_LOW_FLAG_NAME = "AEROSOL_LOW";
+    public static final String STATE_AEROSOL_AVERAGE_FLAG_NAME = "AEROSOL_AVERAGE";
+    public static final String STATE_AEROSOL_HIGH_FLAG_NAME = "AEROSOL_HIGH";
+
+    public static final String STATE_CIRRUS_NONE_FLAG_NAME = "CIRRUS_NONE";
+    public static final String STATE_CIRRUS_SMALL_FLAG_NAME = "CIRRUS_SMALL";
+    public static final String STATE_CIRRUS_AVERAGE_FLAG_NAME = "CIRRUS_AVERAGE";
+    public static final String STATE_CIRRUS_HIGH_FLAG_NAME = "CIRRUS_HIGH";
+
+    public static final String STATE_INTERNAL_CLOUD_ALGORITHM_FLAG_NAME = "INTERNAL_CLOUD_ALGORITHM";
+    public static final String STATE_INTERNAL_FIRE_ALGORITHM_FLAG_NAME = "INTERNAL_FIRE_ALGORITHM";
+    public static final String STATE_MOD35_SNOW_ICE_FLAG_NAME = "MOD35_SNOW_ICE";
+    public static final String STATE_CLOUD_ADJACENCY_FLAG_NAME = "CLOUD_ADJACENCY";
+    public static final String STATE_SALT_PAN_FLAG_NAME = "SALT_PAN";
+    public static final String STATE_INTERNAL_SNOW_MASK_FLAG_NAME = "SNOW";
+
+    public static final String STATE_CLEAR_FLAG_DESCR = "CLEAR";
+    public static final String STATE_CLOUD_FLAG_DESCR = "CLOUDY";
+    public static final String STATE_CLOUD_MIXED_FLAG_DESCR = "CLOUD_MIXED";
+    public static final String STATE_CLOUD_UNKNOWN_FLAG_DESCR = "CLOUD_UNKNOWN";
+    public static final String STATE_CLOUD_SHADOW_FLAG_DESCR = "CLOUD_SHADOW";
+
+    public static final String STATE_SHALLOW_OCEAN_FLAG_DESCR = "SHALLOW_OCEAN";
+    public static final String STATE_LAND_FLAG_DESCR = "LAND";
+    public static final String STATE_COASTLINE_FLAG_DESCR = "COASTLINE";
+    public static final String STATE_SHALLOW_INLAND_WATER_FLAG_DESCR = "SHALLOW_INLAND_WATER";
+    public static final String STATE_EPHEMERAL_WATER_FLAG_DESCR = "EPHEMERAL";
+    public static final String STATE_DEEP_INLAND_WATER_FLAG_DESCR = "DEEP_INLAND_WATER";
+    public static final String STATE_MODERATE_OCEAN_FLAG_DESCR = "MODERATE_OCEAN";
+    public static final String STATE_DEEP_OCEAN_FLAG_DESCR = "DEEP_OCEAN";
+
+    public static final String STATE_AEROSOL_CLIMATOLOGY_FLAG_DESCR = "AEROSOL_CLIMATOLOGY";
+    public static final String STATE_AEROSOL_LOW_FLAG_DESCR = "AEROSOL_LOW";
+    public static final String STATE_AEROSOL_AVERAGE_FLAG_DESCR = "AEROSOL_AVERAGE";
+    public static final String STATE_AEROSOL_HIGH_FLAG_DESCR = "AEROSOL_HIGH";
+
+    public static final String STATE_CIRRUS_NONE_FLAG_DESCR = "CIRRUS_NONE";
+    public static final String STATE_CIRRUS_SMALL_FLAG_DESCR = "CIRRUS_SMALL";
+    public static final String STATE_CIRRUS_AVERAGE_FLAG_DESCR = "CIRRUS_AVERAGE";
+    public static final String STATE_CIRRUS_HIGH_FLAG_DESCR = "CIRRUS_HIGH";
+
+    public static final String STATE_INTERNAL_CLOUD_ALGORITHM_FLAG_DESCR = "INTERNAL_CLOUD_ALGORITHM";
+    public static final String STATE_INTERNAL_FIRE_ALGORITHM_FLAG_DESCR = "INTERNAL_FIRE_ALGORITHM";
+    public static final String STATE_MOD35_SNOW_ICE_FLAG_DESCR = "MOD35_SNOW_ICE";
+    public static final String STATE_CLOUD_ADJACENCY_FLAG_DESCR = "CLOUD_ADJACENCY";
+    public static final String STATE_SALT_PAN_FLAG_DESCR = "SALT_PAN";
+    public static final String STATE_INTERNAL_SNOW_MASK_FLAG_DESCR = "SNOW";
+
+
+    public static final Color[] FLAG_COLORS = {
+            new Color(120, 255, 180),
+            new Color(255, 255, 0),
+            new Color(0, 255, 255),
+            new Color(255, 100, 0),
+            new Color(255, 255, 180),
+            new Color(255, 0, 255),
+            new Color(0, 0, 255),
+            new Color(180, 180, 255),
+            new Color(255, 150, 100),
+            new Color(0, 255, 0)
+    };
+
 }
--- beam-globalbedo-bbdr-retrieval/src/main/java/org/esa/beam/globalbedo/bbdr/Modis09StatusBitMaskOp.java
+++ beam-globalbedo-bbdr-retrieval/src/main/java/org/esa/beam/globalbedo/bbdr/Modis09StatusBitMaskOp.java
@@ -1,21 +1,22 @@
 package org.esa.beam.globalbedo.bbdr;
 
-import org.esa.beam.dataio.ProbaVConstants;
-import org.esa.beam.framework.datamodel.Band;
-import org.esa.beam.framework.datamodel.Product;
-import org.esa.beam.framework.datamodel.ProductData;
+import org.esa.beam.framework.datamodel.*;
 import org.esa.beam.framework.gpf.OperatorException;
 import org.esa.beam.framework.gpf.OperatorSpi;
+import org.esa.beam.framework.gpf.annotations.OperatorMetadata;
 import org.esa.beam.framework.gpf.annotations.SourceProduct;
 import org.esa.beam.framework.gpf.pointop.*;
 import org.esa.beam.util.BitSetter;
 import org.esa.beam.util.ProductUtils;
 
+import java.awt.*;
+
 /**
- * todo: add comment
+ * Extracts pixel status from MOD09 state band into a new flag band
  *
  * @author olafd
  */
+@OperatorMetadata(alias = "ga.l2.mod09.state")
 public class Modis09StatusBitMaskOp extends PixelOperator {
 
     private static final int SRC_FLAG = 0;
@@ -24,27 +25,34 @@ public class Modis09StatusBitMaskOp extends PixelOperator {
     @SourceProduct
     private Product sourceProduct;
 
-    private static final String TARGET_FLAG_BAND_NAME = Mod09Constants.MOD09_STATE_BAND_NAME;
+    private static final String TARGET_FLAG_BAND_NAME = Mod09Constants.MOD09_STATE_FLAG_BAND_NAME;
 
 
     @Override
     protected void configureTargetProduct(ProductConfigurer productConfigurer) {
         super.configureTargetProduct(productConfigurer);
+        final Product targetProduct = productConfigurer.getTargetProduct();
 
-        ProductUtils.copyMetadata(sourceProduct, getTargetProduct());
-        ProductUtils.copyMasks(sourceProduct, getTargetProduct());
-        ProductUtils.copyGeoCoding(sourceProduct, getTargetProduct());
+        ProductUtils.copyMetadata(sourceProduct, targetProduct);
+        ProductUtils.copyMasks(sourceProduct, targetProduct);
+        ProductUtils.copyGeoCoding(sourceProduct, targetProduct);
         for (Band b: sourceProduct.getBands()) {
-            ProductUtils.copyBand(b.getName(), sourceProduct, getTargetProduct(), true);
+            ProductUtils.copyBand(b.getName(), sourceProduct,targetProduct, true);
         }
 
-        final Product targetProduct = productConfigurer.getTargetProduct();
-        targetProduct.addBand(TARGET_FLAG_BAND_NAME, ProductData.TYPE_INT16);
+        FlagCoding stateFlagCoding = new FlagCoding(Mod09Constants.MOD09_STATE_FLAG_BAND_NAME);
+        addStateQualityFlags(stateFlagCoding);
+        addStateQualityMasks(targetProduct);
+        targetProduct.getFlagCodingGroup().add(stateFlagCoding);
+
+        final Band stateFlagBand = targetProduct.addBand(TARGET_FLAG_BAND_NAME, ProductData.TYPE_INT32);
+        stateFlagBand.setDescription("MODIS MOD09 State Flags");
+        stateFlagBand.setSampleCoding(stateFlagCoding);
     }
 
     @Override
     protected void configureSourceSamples(SampleConfigurer sampleConfigurer) throws OperatorException {
-        sampleConfigurer.defineSample(SRC_FLAG, Mod09Constants.MOD09_STATE_FLAG_BAND_NAME);
+        sampleConfigurer.defineSample(SRC_FLAG, Mod09Constants.MOD09_STATE_BAND_NAME);
     }
 
     @Override
@@ -59,69 +67,230 @@ public class Modis09StatusBitMaskOp extends PixelOperator {
     }
 
     private void computeModis09StateMask(int srcValue, WritableSample[] targetSamples) {
+        targetSamples[TRG_FLAG].set(Mod09Constants.STATE_CLEAR_BIT_INDEX, isClear(srcValue));
         targetSamples[TRG_FLAG].set(Mod09Constants.STATE_CLOUD_BIT_INDEX, isCloud(srcValue));
+        targetSamples[TRG_FLAG].set(Mod09Constants.STATE_CLOUD_MIXED_BIT_INDEX, isCloudMixed(srcValue));
+        targetSamples[TRG_FLAG].set(Mod09Constants.STATE_CLOUD_UNKNOWN_BIT_INDEX, isCloudUnknown(srcValue));
+
         targetSamples[TRG_FLAG].set(Mod09Constants.STATE_CLOUD_SHADOW_BIT_INDEX, isCloudShadow(srcValue));
-        targetSamples[TRG_FLAG].set(Mod09Constants.STATE_LAND_WATER_BIT_INDEX, isUndefined(srcValue));
-        targetSamples[TRG_FLAG].set(Mod09Constants.STATE_AEROSOL_QUANTITY_BIT_INDEX, isUndefined(srcValue));
-        targetSamples[TRG_FLAG].set(Mod09Constants.STATE_CIRRUS_BIT_INDEX, isUndefined(srcValue));
-        targetSamples[TRG_FLAG].set(Mod09Constants.STATE_INTERNAL_CLOUD_ALGORITHM_BIT_INDEX, isUndefined(srcValue));
-        targetSamples[TRG_FLAG].set(Mod09Constants.STATE_INTERNAL_FIRE_ALGORITHM_BIT_INDEX, isUndefined(srcValue));
-        targetSamples[TRG_FLAG].set(Mod09Constants.STATE_MOD35_SNOW_ICE_BIT_INDEX, isUndefined(srcValue));
-        targetSamples[TRG_FLAG].set(Mod09Constants.STATE_CLOUD_ADJACENCY_BIT_INDEX, isUndefined(srcValue));
-        targetSamples[TRG_FLAG].set(Mod09Constants.STATE_SALT_PAN_BIT_INDEX, isUndefined(srcValue));
-        targetSamples[TRG_FLAG].set(Mod09Constants.STATE_INTERNAL_SNOW_MASK_BIT_INDEX, isUndefined(srcValue));
+
+        targetSamples[TRG_FLAG].set(Mod09Constants.STATE_SHALLOW_OCEAN_BIT_INDEX, isShallowOcean(srcValue));
+        targetSamples[TRG_FLAG].set(Mod09Constants.STATE_LAND_BIT_INDEX, isLand(srcValue));
+        targetSamples[TRG_FLAG].set(Mod09Constants.STATE_COASTLINE_BIT_INDEX, isCoastline(srcValue));
+        targetSamples[TRG_FLAG].set(Mod09Constants.STATE_SHALLOW_INLAND_WATER_BIT_INDEX, isShallowInlandWater(srcValue));
+        targetSamples[TRG_FLAG].set(Mod09Constants.STATE_EPHEMERAL_WATER_BIT_INDEX, isEphemeralWater(srcValue));
+        targetSamples[TRG_FLAG].set(Mod09Constants.STATE_DEEP_INLAND_WATER_BIT_INDEX, isDeepInlandWater(srcValue));
+        targetSamples[TRG_FLAG].set(Mod09Constants.STATE_MODERATE_OCEAN_BIT_INDEX, isModerateOcean(srcValue));
+        targetSamples[TRG_FLAG].set(Mod09Constants.STATE_DEEP_OCEAN_BIT_INDEX, isDeepOcean(srcValue));
+
+        targetSamples[TRG_FLAG].set(Mod09Constants.STATE_AEROSOL_CLIMATOLOGY_BIT_INDEX, isAerosolClimatology(srcValue));
+        targetSamples[TRG_FLAG].set(Mod09Constants.STATE_AEROSOL_LOW_BIT_INDEX, isAerosolLow(srcValue));
+        targetSamples[TRG_FLAG].set(Mod09Constants.STATE_AEROSOL_AVERAGE_BIT_INDEX, isAerosolAverage(srcValue));
+        targetSamples[TRG_FLAG].set(Mod09Constants.STATE_AEROSOL_HIGH_BIT_INDEX, isAerosolHigh(srcValue));
+
+        targetSamples[TRG_FLAG].set(Mod09Constants.STATE_CIRRUS_NONE_BIT_INDEX, isCirrusNone(srcValue));
+        targetSamples[TRG_FLAG].set(Mod09Constants.STATE_CIRRUS_SMALL_BIT_INDEX, isCirrusSmall(srcValue));
+        targetSamples[TRG_FLAG].set(Mod09Constants.STATE_CIRRUS_AVERAGE_BIT_INDEX, isCirrusAverage(srcValue));
+        targetSamples[TRG_FLAG].set(Mod09Constants.STATE_CIRRUS_HIGH_BIT_INDEX, isCirrusHigh(srcValue));
+
+        targetSamples[TRG_FLAG].set(Mod09Constants.STATE_INTERNAL_CLOUD_ALGORITHM_BIT_INDEX, isInternalCloudAlgorithm(srcValue));
+        targetSamples[TRG_FLAG].set(Mod09Constants.STATE_INTERNAL_FIRE_ALGORITHM_BIT_INDEX, isInternalFireAlgorithm(srcValue));
+        targetSamples[TRG_FLAG].set(Mod09Constants.STATE_MOD35_SNOW_ICE_BIT_INDEX, isMod35SnowIce(srcValue));
+        targetSamples[TRG_FLAG].set(Mod09Constants.STATE_CLOUD_ADJACENCY_BIT_INDEX, isCloudAdjacent(srcValue));
+        targetSamples[TRG_FLAG].set(Mod09Constants.STATE_SALT_PAN_BIT_INDEX, isSaltPan(srcValue));
+        targetSamples[TRG_FLAG].set(Mod09Constants.STATE_INTERNAL_SNOW_MASK_BIT_INDEX, isSnow(srcValue));
 
     }
 
+    // bits 0-1:
     private boolean isClear(int srcValue) {
         return (!BitSetter.isFlagSet(srcValue, 0) &&
-                !BitSetter.isFlagSet(srcValue, 1) &&
-                !BitSetter.isFlagSet(srcValue, 2));
-    }
-
-    private boolean isUndefined(int srcValue) {
-        return (!BitSetter.isFlagSet(srcValue, 0) &&
-                BitSetter.isFlagSet(srcValue, 1) &&
-                !BitSetter.isFlagSet(srcValue, 2));
+                !BitSetter.isFlagSet(srcValue, 1));
     }
 
     private boolean isCloud(int srcValue) {
         return (BitSetter.isFlagSet(srcValue, 0) &&
-                BitSetter.isFlagSet(srcValue, 1) &&
-                !BitSetter.isFlagSet(srcValue, 2));
+                !BitSetter.isFlagSet(srcValue, 1));
     }
 
-    private boolean isSnowIce(int srcValue) {
+    private boolean isCloudMixed(int srcValue) {
         return (!BitSetter.isFlagSet(srcValue, 0) &&
-                !BitSetter.isFlagSet(srcValue, 1) &&
-                BitSetter.isFlagSet(srcValue, 2));
+                BitSetter.isFlagSet(srcValue, 1));
     }
 
-    private boolean isCloudShadow(int srcValue) {
+    private boolean isCloudUnknown(int srcValue) {
         return (BitSetter.isFlagSet(srcValue, 0) &&
-                !BitSetter.isFlagSet(srcValue, 1) &&
-                !BitSetter.isFlagSet(srcValue, 2));
+                BitSetter.isFlagSet(srcValue, 1));
+    }
+
+    // bit 2:
+    private boolean isCloudShadow(int srcValue) {
+        return (BitSetter.isFlagSet(srcValue, 2));
+    }
+
+    // bits 3-5:
+    private boolean isShallowOcean(int srcValue) {
+        return (!BitSetter.isFlagSet(srcValue, 3) &&
+                !BitSetter.isFlagSet(srcValue, 4) &&
+                !BitSetter.isFlagSet(srcValue, 5));
     }
 
     private boolean isLand(int srcValue) {
-        return BitSetter.isFlagSet(srcValue, 3);
+        return (BitSetter.isFlagSet(srcValue, 3) &&
+                !BitSetter.isFlagSet(srcValue, 4) &&
+                !BitSetter.isFlagSet(srcValue, 5));
     }
 
-    private boolean isGoodSwir(int srcValue) {
-        return BitSetter.isFlagSet(srcValue, 4);
+    private boolean isCoastline(int srcValue) {
+        return (!BitSetter.isFlagSet(srcValue, 3) &&
+                BitSetter.isFlagSet(srcValue, 4) &&
+                !BitSetter.isFlagSet(srcValue, 5));
     }
 
-    private boolean isGoodNir(int srcValue) {
-        return BitSetter.isFlagSet(srcValue, 5);
+    private boolean isShallowInlandWater(int srcValue) {
+        return (BitSetter.isFlagSet(srcValue, 3) &&
+                BitSetter.isFlagSet(srcValue, 4) &&
+                !BitSetter.isFlagSet(srcValue, 5));
     }
 
-    private boolean isGoodRed(int srcValue) {
-        return BitSetter.isFlagSet(srcValue, 6);
+    private boolean isEphemeralWater(int srcValue) {
+        return (!BitSetter.isFlagSet(srcValue, 3) &&
+                !BitSetter.isFlagSet(srcValue, 4) &&
+                BitSetter.isFlagSet(srcValue, 5));
     }
 
-    private boolean isGoodBlue(int srcValue) {
-        return BitSetter.isFlagSet(srcValue, 7);
+    private boolean isDeepInlandWater(int srcValue) {
+        return (BitSetter.isFlagSet(srcValue, 3) &&
+                !BitSetter.isFlagSet(srcValue, 4) &&
+                BitSetter.isFlagSet(srcValue, 5));
     }
+
+    private boolean isModerateOcean(int srcValue) {
+        return (!BitSetter.isFlagSet(srcValue, 3) &&
+                BitSetter.isFlagSet(srcValue, 4) &&
+                BitSetter.isFlagSet(srcValue, 5));
+    }
+
+    private boolean isDeepOcean(int srcValue) {
+        return (BitSetter.isFlagSet(srcValue, 3) &&
+                BitSetter.isFlagSet(srcValue, 4) &&
+                BitSetter.isFlagSet(srcValue, 5));
+    }
+
+    // bits 6-7:
+    private boolean isAerosolClimatology(int srcValue) {
+        return (!BitSetter.isFlagSet(srcValue, 6) &&
+                !BitSetter.isFlagSet(srcValue, 7));
+    }
+
+    private boolean isAerosolLow(int srcValue) {
+        return (BitSetter.isFlagSet(srcValue, 6) &&
+                !BitSetter.isFlagSet(srcValue, 7));
+    }
+
+    private boolean isAerosolAverage(int srcValue) {
+        return (!BitSetter.isFlagSet(srcValue, 6) &&
+                BitSetter.isFlagSet(srcValue, 7));
+    }
+
+    private boolean isAerosolHigh(int srcValue) {
+        return (BitSetter.isFlagSet(srcValue, 6) &&
+                BitSetter.isFlagSet(srcValue, 7));
+    }
+
+    // bits 8-9:
+    private boolean isCirrusNone(int srcValue) {
+        return (!BitSetter.isFlagSet(srcValue, 8) &&
+                !BitSetter.isFlagSet(srcValue, 9));
+    }
+
+    private boolean isCirrusSmall(int srcValue) {
+        return (BitSetter.isFlagSet(srcValue, 8) &&
+                !BitSetter.isFlagSet(srcValue, 9));
+    }
+
+    private boolean isCirrusAverage(int srcValue) {
+        return (!BitSetter.isFlagSet(srcValue, 8) &&
+                BitSetter.isFlagSet(srcValue, 9));
+    }
+
+    private boolean isCirrusHigh(int srcValue) {
+        return (BitSetter.isFlagSet(srcValue, 8) &&
+                BitSetter.isFlagSet(srcValue, 9));
+    }
+
+    // bit 10:
+    private boolean isInternalCloudAlgorithm(int srcValue) {
+        return (BitSetter.isFlagSet(srcValue, 10));
+    }
+
+    // bit 11:
+    private boolean isInternalFireAlgorithm(int srcValue) {
+        return (BitSetter.isFlagSet(srcValue, 11));
+    }
+
+    // bit 12:
+    private boolean isMod35SnowIce(int srcValue) {
+        return (BitSetter.isFlagSet(srcValue, 12));
+    }
+
+    // bit 13:
+    private boolean isCloudAdjacent(int srcValue) {
+        return (BitSetter.isFlagSet(srcValue, 13));
+    }
+
+    // bit 14:
+    private boolean isSaltPan(int srcValue) {
+        return (BitSetter.isFlagSet(srcValue, 14));
+    }
+
+    // bit 15:
+    private boolean isSnow(int srcValue) {
+        return (BitSetter.isFlagSet(srcValue, 15));
+    }
+
+    public static void addStateQualityMasks(Product probavProduct) {
+        ProductNodeGroup<Mask> maskGroup = probavProduct.getMaskGroup();
+        addMask(probavProduct, maskGroup, Mod09Constants.MOD09_STATE_FLAG_BAND_NAME,
+                Mod09Constants.STATE_CLOUD_FLAG_NAME,
+                Mod09Constants.STATE_CLOUD_FLAG_DESCR,
+                Mod09Constants.FLAG_COLORS[1], 0.5f);
+        addMask(probavProduct, maskGroup, Mod09Constants.MOD09_STATE_FLAG_BAND_NAME,
+                Mod09Constants.STATE_LAND_FLAG_NAME,
+                Mod09Constants.STATE_LAND_FLAG_DESCR,
+                Mod09Constants.FLAG_COLORS[9], 0.5f);
+        addMask(probavProduct, maskGroup, Mod09Constants.MOD09_STATE_FLAG_BAND_NAME,
+                Mod09Constants.STATE_MOD35_SNOW_ICE_FLAG_NAME,
+                Mod09Constants.STATE_MOD35_SNOW_ICE_FLAG_DESCR,
+                Mod09Constants.FLAG_COLORS[5], 0.5f);
+        // todo: complete
+    }
+
+    public static void addStateQualityFlags(FlagCoding probavSmFlagCoding) {
+        probavSmFlagCoding.addFlag(Mod09Constants.STATE_CLOUD_FLAG_NAME,
+                BitSetter.setFlag(0, Mod09Constants.STATE_CLOUD_BIT_INDEX),
+                Mod09Constants.STATE_CLOUD_FLAG_DESCR);
+        probavSmFlagCoding.addFlag(Mod09Constants.STATE_LAND_FLAG_NAME,
+                BitSetter.setFlag(0, Mod09Constants.STATE_LAND_BIT_INDEX),
+                Mod09Constants.STATE_LAND_FLAG_DESCR);
+        probavSmFlagCoding.addFlag(Mod09Constants.STATE_MOD35_SNOW_ICE_FLAG_NAME,
+                BitSetter.setFlag(0, Mod09Constants.STATE_MOD35_SNOW_ICE_BIT_INDEX),
+                Mod09Constants.STATE_MOD35_SNOW_ICE_FLAG_DESCR);
+        // todo: complete
+    }
+
+    private static void addMask(Product mod35Product, ProductNodeGroup<Mask> maskGroup,
+                                String bandName, String flagName, String description, Color color, float transparency) {
+        int width = mod35Product.getSceneRasterWidth();
+        int height = mod35Product.getSceneRasterHeight();
+        String maskPrefix = "";
+        Mask mask = Mask.BandMathsType.create(maskPrefix + flagName,
+                description, width, height,
+                bandName + "." + flagName,
+                color, transparency);
+        maskGroup.add(mask);
+    }
+
 
     public static class Spi extends OperatorSpi {
         public Spi() {
